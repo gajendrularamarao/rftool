b44b9327142a4e1b7e1af326966610e6eda1aa22
RamaRao <ramarao_gajendrula@gmail.com>
2019-06-24 02:02:15 -0700

    update code for deploying to tomcat

--- src/main/java/com/rf/rftool/RftoolApplication.java
+++ src/main/java/com/rf/rftool/RftoolApplication.java
@@ -2,10 +2,17 @@ package com.rf.rftool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RftoolApplication {
+public class RftoolApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RftoolApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RftoolApplication.class, args);
 	}
